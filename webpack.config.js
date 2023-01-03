1fba53041bfd3afd75cc2f9e71f8c4b121035492
Haris Souici <44004478+simtaxx@users.noreply.github.com>
2023-01-03 01:16:00 +0100

    [FEATURE] Babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,24 @@ module.exports = {
     path: __dirname + '/dist',
     filename: 'bundle.js'
   },
+  devtool: 'eval',
   devServer: {
     static: path.join(__dirname, 'dist')
+  },
+  "module": {
+    "rules": [
+      {
+        "test": /\.js$/,
+        "exclude": /node_modules/,
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            "presets": [
+              "@babel/preset-env",
+            ]
+          }
+        }
+      },
+    ]
   }
 }
